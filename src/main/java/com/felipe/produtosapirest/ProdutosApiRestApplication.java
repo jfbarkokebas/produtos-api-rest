149f4956bbc579b2541be49a051c85b4a65269a8
felipe <jfbarkokebas@gmail.com>
2024-03-04 18:06:40 -0300

    first commit

---
+++ src/main/java/com/felipe/produtosapirest/ProdutosApiRestApplication.java
@@ -0,0 +1,14 @@
+package com.felipe.produtosapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosApiRestApplication.class, args);
+		System.out.println(" *** Buid Success ***");
+	}
+
+}
